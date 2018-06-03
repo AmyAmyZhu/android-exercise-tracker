3e69d29ae39c349deed56b11d34220a3b18eefd4
ZeFangZhu <zefang.zhu@mapsted.com>
2018-06-03 02:14:41 -0400

    contribution

--- app/src/main/java/com/amyzhu/android_exercise_tracker/fragments/HomeFragment.java
+++ app/src/main/java/com/amyzhu/android_exercise_tracker/fragments/HomeFragment.java
@@ -1,16 +1,20 @@
 package com.amyzhu.android_exercise_tracker.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.amyzhu.android_exercise_tracker.R;
 
 public class HomeFragment extends Fragment {
+
+    Integer daysWorkOut = 30;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -26,5 +30,17 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        LinearLayout contributionLL = (LinearLayout) view.findViewById(R.id.contributionLL);
+        for(int i = 0; i < daysWorkOut; i++) {
+            final ImageView imageView = new ImageView(getContext());
+            imageView.setImageResource(R.drawable.ic_one_contribution_20dp);
+            imageView.setId(i);
+            contributionLL.addView(imageView);
+        }
     }
 }
